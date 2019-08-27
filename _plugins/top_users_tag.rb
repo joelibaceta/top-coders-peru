c65c3c8176876023767848dd32d2b8702db5cc83
joel ibaceta <mail@joelibaceta.com>
2019-08-26 20:03:47 -0500

    test

--- _plugins/top_users_tag.rb
+++ _plugins/top_users_tag.rb
@@ -6,45 +6,55 @@ require 'uri'
 module Jekyll
     class MeetupMembersCounterTag < Liquid::Tag
 
-        
+        def authorization_string
+            return "client_id=#{ENV['CLIENT_ID']}&client_secret=#{ENV['CLIENT_SECRET']}"
+        end
 
         def countCommits(user)
-            uri = URI.parse("https://api.github.com/search/commits?q=author-name:#{user}")
+            uri = URI.parse("https://api.github.com/search/commits?q=author-name:#{user}&#{authorization_string}")
             response = Net::HTTP.get_response(uri)
             commits = JSON.parse(response.body) 
             return commits["total_count"]
         end
 
         def countStarts(user)
-            uri = URI.parse("https://api.github.com/search/repositories?q=user:#{user} stars:>0")
+            uri = URI.parse("https://api.github.com/search/repositories?q=user:#{user} stars:>0&#{authorization_string}")
             response = Net::HTTP.get_response(uri)
             repos = JSON.parse(response.body) 
             counter = 0
-            
+            p uri
             repos["items"].each do |repo| 
                 counter += (repo["stargazers_count"]).to_i
             end
             return counter
         end
 
+        def getUserData(user)
+            uri = URI.parse("https://api.github.com/users/#{user}?#{authorization_string}")
+            response = Net::HTTP.get_response(uri)
+            user = JSON.parse(response.body) 
+             
+            return user
+        end
+
         def getTopUsersData
             @top_users = {}
 
-            uri = URI.parse("https://api.github.com/search/users?q=location:lima")
+            uri = URI.parse("https://api.github.com/search/users?q=location:lima&#{authorization_string}")
+            p uri
             response = Net::HTTP.get_response(uri)
             users = JSON.parse(response.body)
 
-            
-
             users["items"].each do |user|
+
+                data = getUserData(user["login"])
                 
-                p user["login"]
                 @top_users[user["login"]] = {
-                    name: user["name"],
-                    email: user["email"],
-                    company: user["company"],
-                    followers: user["followers"],
-                    url: user["html_url"],
+                    name: data["name"],
+                    email: data["email"],
+                    company: data["company"],
+                    followers: data["followers"],
+                    url: data["html_url"],
                     commits: countCommits(user["login"]),
                     stars: countStarts(user["login"])
                 }
