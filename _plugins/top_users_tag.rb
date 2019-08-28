1a67fedb22df9602dcc234d9990732c91ca230d6
Sergio Xalambrí <hello@sergiodxa.com>
2019-08-28 13:13:51 -0500

    Add method to count user issues and PRs

--- _plugins/top_users_tag.rb
+++ _plugins/top_users_tag.rb
@@ -10,6 +10,22 @@ module Jekyll
             return "client_id=#{ENV['CLIENT_ID']}&client_secret=#{ENV['CLIENT_SECRET']}"
         end
 
+        def countIssues(user)
+            uri = URI.parse("https://api.github.com/search/issues?q=author:#{user}&#{authorization_string}")
+
+            http = Net::HTTP.new(uri.host, uri.port)
+            http.use_ssl = true
+
+            request = Net::HTTP::Get.new(uri)
+            request["Accept"] = 'application/vnd.github.cloak-preview'
+
+            response = http.request(request)
+
+            issues = JSON.parse(response.body)
+
+            issues["total_count"]
+        end
+
         def countRepos(user)
             uri = URI.parse("https://api.github.com/users/#{user}/repos?#{authorization_string}")
 
@@ -74,6 +90,7 @@ module Jekyll
             max_stars = 0
             max_followers = 0
             max_public_repos = 0
+            max_issues = 0
             
             (1..3).each do |i|
 
@@ -96,11 +113,13 @@ module Jekyll
                     stars = countStarts(user["login"])
                     followers = data["followers"]
                     repos = countRepos(user["login"])
+                    issues = countIssues(user["login"])
 
                     max_commits = commits if commits > max_commits
                     max_stars = stars if stars > max_stars
                     max_followers = followers if followers > max_followers
                     max_public_repos = repos if repos > max_public_repos
+                    max_issues = issues if issues > max_issues
                     
                     @top_users << {
                         id: user["login"],
@@ -112,14 +131,21 @@ module Jekyll
                         repos: repos,
                         url: data["html_url"],
                         commits: commits,
-                        stars: stars
+                        stars: stars,
+                        issues: issues
                     }
                 end
 
             end
             
             @top_users.each do |user|
-                user[:score] = (user[:commits] / max_commits.to_f + user[:stars] / max_stars.to_f + user[:followers] / max_followers.to_f + user[:repos] / max_public_repos.to_f) / 4.0
+                user[:score] = (
+                    user[:commits] / max_commits.to_f +
+                    user[:stars] / max_stars.to_f +
+                    user[:followers] / max_followers.to_f +
+                    user[:repos] / max_public_repos.to_f +
+                    user[:issues] / max_issues.to_f
+                ) / 5.0
             end
 
             return @top_users.sort_by {|obj| obj[:score]}.reverse
@@ -128,13 +154,13 @@ module Jekyll
         def render(context)
             users = getTopUsersData
             element = "<table>\n"
-            element += "<thead><th><td colspan='2'>user</td><td>name</td><td>email</td><td>company</td><td>followers</td><td>commits</td><td>stars</td><td>repos</td></th><thead>\n"
+            element += "<thead><th><td colspan='2'>User</td><td>Name</td><td>Email</td><td>Company</td><td>Followers</td><td>Commits</td><td>Stars</td><td>Repos</td><td>Issues/PR</td></th><thead>\n"
             element += "<tbody>"
             users.each_with_index do |user, i|
                 element += "<tr><td>#{i + 1}</td>"
                 element += "<td><img  width='60px' src='#{user[:pic]}'></td>"
                 element += "<td><a href='#{user[:url]}'>#{user[:id]}</a></td><td>#{user[:name]}</td><td>#{user[:email]}</td><td>#{user[:company]}</td>"
-                element += "<td>#{user[:followers]}</td><td>#{user[:commits]}</td><td>#{user[:stars]}</td><td>#{user[:repos]}</td></tr>\n"
+                element += "<td>#{user[:followers]}</td><td>#{user[:commits]}</td><td>#{user[:stars]}</td><td>#{user[:repos]}</td><td>#{user[:issues]}</td></tr>\n"
             end
             element += "</tbody>"
             element += "</table>\n"
