a2e8e2b317aa7531a04fcda35c8f7f4a978bd192
Joel Ibaceta <mail@joelibaceta.com>
2019-09-22 14:21:21 -0500

    Update top_users_tag.rb

--- _plugins/top_users_tag.rb
+++ _plugins/top_users_tag.rb
@@ -114,7 +114,7 @@ module Jekyll
 
                 sleep(30)
 
-                uri = URI.parse("https://api.github.com/search/users?q=location:lima location:peru followers:>10&per_page=30&page=#{i}&sort=followers&order=desc&#{authorization_string}")
+                uri = URI.parse("https://api.github.com/search/users?q=location:lima location:peru followers:>10&per_page=25&page=#{i}&sort=followers&order=desc&#{authorization_string}")
 
                 response = Net::HTTP.get_response(uri)
                 users = JSON.parse(response.body)
