59a459d8bbcf8359d059cb9ef6c1cdd8ec3309a1
Joel Ibaceta <mail@joelibaceta.com>
2020-08-05 03:54:44 -0500

    avoid division

--- _plugins/top_users_tag.rb
+++ _plugins/top_users_tag.rb
@@ -121,11 +121,11 @@ module Jekyll
             end
             @top_users.each do |user|
                 user[:score] = (
-                    ((user[:commits] / max_commits.to_f)) * 0.3
+                    (user[:commits] / max_commits.to_f) * 0.3
                     (user[:stars] / max_stars.to_f) * 0.2 +
                     (user[:followers] / max_followers.to_f) * 0.3 +
                     (user[:repos] / max_public_repos.to_f) * 0.2 
-                ) / 4.0
+                )
             end
 
             languages = @technologies.sort_by {|k,v| v}.reverse.first(15).to_h 
